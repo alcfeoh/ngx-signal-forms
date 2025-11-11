e0e0f7ad99f7267fde603d1c0cd917622683b7f2
alcfeoh <alain@interstate21.com>
2025-11-11 15:57:55 +0100

    Signal form first attempt

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, signal} from '@angular/core';
+import {Field, form, maxLength, minLength, pattern, required} from '@angular/forms/signals';
+import {JsonPipe} from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    Field,
+    JsonPipe
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected title = 'form-fun';
+
+  userInfo = signal({
+    firstName: "",
+    lastName: "",
+    street: "",
+    zip: "",
+    city: "",
+    cc: ""
+  });
+
+  userForm = form(this.userInfo, (path) => {
+    required(path.firstName);
+    required(path.zip);
+    pattern(path.zip, new RegExp("[0-9]{5}"));
+    required(path.cc);
+    minLength(path.cc, 16);
+    maxLength(path.cc, 16);
+  });
+
+  logForm(event: Event) {
+    event.preventDefault();
+    console.log(this.userForm().value());
+  }
+
 }
